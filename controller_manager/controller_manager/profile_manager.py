ca7a05d4692f76bdb64613c6ec6904da269a0184
Sai Kishor Kothakota <sai.kishor@pal-robotics.com>
2025-10-18 02:40:57 +0200

    WIP: profile manager

---
+++ controller_manager/controller_manager/profile_manager.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python3
+# Copyright 2025 PAL Robotics S.L.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import argparse
+import errno
+import os
+import sys
+import time
+import warnings
+
+from controller_manager import (
+    configure_controller,
+    list_controllers,
+    load_controller,
+    switch_controllers,
+    unload_controller,
+    set_controller_parameters,
+    set_controller_parameters_from_param_files,
+    bcolors,
+)
+from controller_manager_msgs.msg import ControllerManagerActivity
+from controller_manager_msgs.srv import SwitchController, SwitchProfiles
+from controller_manager.controller_manager_services import ServiceNotFoundError
+
+from filelock import Timeout, FileLock
+import rclpy
+from rclpy.node import Node
+from rclpy.signals import SignalHandlerOptions
+
+
+def first_match(iterable, predicate):
+    return next((n for n in iterable if predicate(n)), None)
+
+
+def combine_name_and_namespace(name_and_namespace):
+    node_name, namespace = name_and_namespace
+    return namespace + ("" if namespace.endswith("/") else "/") + node_name
+
+
+def find_node_and_namespace(node, full_node_name):
+    node_names_and_namespaces = node.get_node_names_and_namespaces()
+    return first_match(
+        node_names_and_namespaces, lambda n: combine_name_and_namespace(
+            n) == full_node_name
+    )
+
+
+def has_service_names(node, node_name, node_namespace, service_names):
+    client_names_and_types = node.get_service_names_and_types_by_node(
+        node_name, node_namespace)
+    if not client_names_and_types:
+        return False
+    client_names, _ = zip(*client_names_and_types)
+    return all(service in client_names for service in service_names)
+
+
+def is_controller_loaded(
+    node, controller_manager, controller_name, service_timeout=0.0, call_timeout=10.0
+):
+    controllers = list_controllers(
+        node, controller_manager, service_timeout, call_timeout
+    ).controller
+    return any(c.name == controller_name for c in controllers)
+
+
+class ControllerManagerProfileService(Node):
+
+    def __init__(self):
+        super().__init__('profile_manager')
+        self.start_srv = self.create_service(
+            SwitchProfiles, '~/switch', self.switch_profile_callback)
+        self.cm_activity = self.create_subscription(
+            ControllerManagerActivity,
+            '/controller_manager/activity',
+            self.activity_callback,
+            rclpy.qos.DurabilityPolicy(1))
+        self.cm_switch_client = self.create_client(
+            SwitchController, '/controller_manager/switch_controller')
+        self.load_profiles()
+
+    def activity_callback(self, msg):
+        print("Current active controllers:")
+        for controllers in msg.controllers:
+            print(f" - {controllers.name}: {controllers.state.label}")
+
+    def load_profiles(self):
+        self.profiles = {}
+        self.profiles["broadcasters"] = ["joint_state_broadcaster"]
+        self.profiles["chain1"] = ["chain1_position_controller"]
+
+    def switch_profile_callback(self, request, response):
+        cm_request = SwitchController.Request()
+        cm_request.activate_controllers = self.profiles[request.start[0]] if request.start else [
+        ]
+        cm_request.deactivate_controllers = self.profiles[request.stop[0]] if request.stop else [
+        ]
+        cm_request.strictness = SwitchController.Request.STRICT
+        cm_request.activate_asap = False
+        cm_request.timeout = rclpy.duration.Duration(seconds=1.0).to_msg()
+        future = self.cm_switch_client.call_async(cm_request)
+        rclpy.spin_until_future_complete(self, future, timeout_sec=5.0)
+        response.ok = future.result().ok
+        response.message = future.result().message
+        print(future.result())
+        return response
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    profile_manager = ControllerManagerProfileService()
+    try:
+        rclpy.spin(profile_manager)
+    except KeyboardInterrupt:
+        pass
+    profile_manager.destroy_node()
+    rclpy.shutdown()
